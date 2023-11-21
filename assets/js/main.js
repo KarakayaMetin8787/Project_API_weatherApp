195fa88e2c931251269e656c3a2603baadc2d480
KarakayaMetin8787 <karakayametin8787@gmail.com>
2023-11-21 10:29:22 +0100

    beide codes in js zusammengefügt

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,26 +4,6 @@ const header = document.body.querySelector('header')
 
 const date = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
 
-fetch ("https://api.openweathermap.org/data/2.5/weather?lat=53.57532&lon=10.01534&units=metric&appid=fca3353fe4f05c026efd359de8542b93")
-
-.then(res => {
-    if(res.ok === false){
-        throw new Error("ok is not true")
-    } return res.json()
-})
-.then(weathArr=>{
-    const obtDate = new Date(weathArr.dt*1000)
-    header.innerHTML = `<h1>Weather in ${weathArr.name}, ${weathArr.sys.country}</h1>
-    <p><img src="http://openweathermap.org/img/w/${weathArr.weather[0].icon}.png" alt="weather icon">${weathArr.main.temp.toFixed(0)}°C</p>
-    <p>${weathArr.weather[0].description}</p>
-
-    <p>Obtained at ${obtDate.getHours()}:${obtDate.getMinutes()}, ${obtDate.getDate()} ${date[obtDate.getMonth()]} ${obtDate.getFullYear()}</p>`
-    console.log();
-    console.log(copyAPI);
-})
-
-console.log()
-
 let backupAPI = {};
 
 const weatherAPI = function getAPI() {
@@ -37,8 +17,14 @@ const weatherAPI = function getAPI() {
     })
     .then(json => {
         
+    const obtDate = new Date(json.dt*1000)
+    header.innerHTML = `<h1>Weather in ${json.name}, ${json.sys.country}</h1>
+    <p><img src="http://openweathermap.org/img/w/${json.weather[0].icon}.png" alt="weather icon">${json.main.temp.toFixed(0)}°C</p>
+    <p>${json.weather[0].description}</p>
+
+    <p>Obtained at ${obtDate.getHours()}:${obtDate.getMinutes()}, ${obtDate.getDate()} ${date[obtDate.getMonth()]} ${obtDate.getFullYear()}</p>`
+
     backupAPI = {...json}
-    console.log(backupAPI)
 
     const options = {
         hour: '2-digit',
